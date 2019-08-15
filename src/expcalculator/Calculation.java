ad83f48397c227e906638055e2915b6b20a641c8
tommy <cy94295@gmail.com>
2019-08-16 00:17:03 +0800

    完成基本功能

--- src/expcalculator/Calculation.java
+++ src/expcalculator/Calculation.java
@@ -2,17 +2,36 @@ package expcalculator;
 
 public class Calculation {
 	private double exp;
+	private int level;
 
-	Calculation(double level){
+	Calculation(double level) {
+		this.level = 0;
+		this.exp = 0;
 	}
 
 	public double getExpCalculated(double level) {
-		if(level > 3) {
-			for(int i = 1; i <= level; i++) {
+		if (level == 0) {
+			return exp;
+		}
+
+		if (level <= 10) {
+			exp += 17;
+		}
+
+		if (level > 10) {
+			for (int i = 1; i <= level; i++) {
 				exp += 174 + (i * 37);
 			}
 		}
-		return (level - 1) * 17;
+
+		if (level > 17) {
+			for (int i = 1; i <= level; i++) {
+				exp += 174 + (i * 37);
+			}
+		}
+
+		return getExpCalculated(level - 1);
+
 	}
 
 }
--- src/expcalculator/Main.java
+++ src/expcalculator/Main.java
@@ -4,13 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int exp = 0;
 		int level = 0;
-		
-		
-		
-		
+
+		Calculation mypet = new Calculation(level);
+		System.out.println(mypet.getExpCalculated(25));
 
 	}
 
