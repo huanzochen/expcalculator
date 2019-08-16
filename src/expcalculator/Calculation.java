b7438b8fc263a9e0511c09960e1f9213972daabd
g <fd>
2019-08-16 17:02:08 +0800

    增加每階段經驗值顯示

--- src/expcalculator/Calculation.java
+++ src/expcalculator/Calculation.java
@@ -2,32 +2,52 @@ package expcalculator;
 
 public class Calculation {
 	private double exp;
-	private final int level;
+	double lvl1Sum,lvl2Sum,lvl3Sum;
+
 
 	Calculation(double level) {
-		this.level = 0;
 		this.exp = 17;
 	}
 
-	public double getExpCalculated(double level) {
+	public double getExpCalculated(int level) {
 		if (level == 0) {
+			System.out.println( "階段所需經驗" + lvl1Sum);
+			System.out.println( "階段所需經驗" + lvl2Sum);
+			System.out.println( "階段所需經驗" + lvl3Sum);
 			return exp;
 		}
 		else if (level <= 10) {
-			exp += (level * 17);
-			System.out.println(level + "等需要" + (level * 17) + "的經驗值");
+			exp += lvl1(level);
+			System.out.println(level + "等需要" + lvl1(level) + "的經驗值");
 		}
 		else if (level > 10 && level <= 17) {
-			exp += ((level * 15) * (level * 0.12));
-			System.out.println(level + "等需要" + ((level * 15) * (level * 0.12)) + "的經驗值");
+			exp += lvl2(level);
+			System.out.println(level + "等需要" + lvl2(level) + "的經驗值");
 		}
 		else if (level > 17 && level <= 25) {
-			exp += ((level * 15) * (level * 0.2));
-			System.out.println(level + "等需要" + ((level * 15) * (level * 0.2)) + "的經驗值");
+			exp += lvl3(level);
+			System.out.println(level + "等需要" + lvl3(level) + "的經驗值");
 		}
 		return getExpCalculated(level - 1);
+	}
 
-
+	public double lvl1 (int level) {
+		double exp = 0;
+		exp += (level * 17);
+		lvl1Sum += (level * 17);
+		return exp;
+	}
+	public double lvl2 (int level) {
+		double exp = 0;
+		exp += ((level * 15) * (level * 0.12));
+		lvl2Sum += (level * 17);
+		return exp;
+	}
+	public double lvl3 (int level) {
+		double exp = 0;
+		exp += ((level * 15) * (level * 0.2));
+		lvl3Sum += (level * 17);
+		return exp;
 	}
 
 }
